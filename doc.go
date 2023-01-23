39092ffdb62765be0017945c7497caf2a6046126
rosshpayne <rosshpayne@gmail.com>
2023-01-23 01:18:38 +0000

    doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// module method-db presents a methods based approach for specifying queries and data mutations for SQL and NoSQL databases.
+// It aims are to be a more productive way to write databsae code in hetrogenious database environments and provide
+// benefits for single database environments.
+package mdb
